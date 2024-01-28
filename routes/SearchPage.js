dce862d3fc49395617a09742abf46cd5cd9d1eeb
jordanp2002 <jordanp2002@live.com>
2024-01-27 20:03:47 -0800

    added basic api connection

--- routes/SearchPage.js
+++ routes/SearchPage.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const pokemon = require('pokemontcgsdk');
+pokemon.configure({apiKey: '${process.env.API_KEY}'});
 
 router.get('/', function(req, res,next) {
-    
-        
-        
+    let pokemonName = req.query.pokemon;
+    pokemon.card.where({ q: 'name:' + pokemonName}).then(result => {
+        res.send(result.data[0].small);
+    })
 });
 module.exports = router;
