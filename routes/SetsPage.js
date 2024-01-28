b0a4432d783dc31dc29c7d48f6fb0b945fd5f3ac
jordanp2002 <jordanp2002@live.com>
2024-01-27 21:06:01 -0800

    added sets SetsPage

---
+++ routes/SetsPage.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const pokemon = require('pokemontcgsdk');
+pokemon.configure({apiKey: '${process.env.API_KEY}'});
+
+router.get('/', function(req, res,next) {
+    res.write('<!DOCTYPE html>');
+    res.write('<html lang="en">');
+    res.write('<head>');
+    res.write('<meta charset="UTF-8">');
+    res.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+    res.write('<title>Pokemon Search</title>');
+    res.write('<style>');
+    res.write('body {');
+    res.write('background-color: #f2f2f2;');
+    res.write('font-family: \'Arial\', sans-serif;');
+    res.write('margin: 0;');
+    res.write('padding: 0;');
+    res.write('}');
+    res.write('header {');
+    res.write('background-color: #e74c3c;');
+    res.write('padding: 15px;');
+    res.write('text-align: center;');
+    res.write('}');
+    res.write('main {');
+    res.write('padding: 20px;');
+    res.write('text-align: center;');
+    res.write('}');
+    res.write('img {');
+    res.write('width: 150px;');
+    res.write('margin: 10px;');
+    res.write('}');
+    res.write('</style>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<header>');
+    res.write('<h1>PokeMon Sets</h1>');
+    res.write('</header>');
+    res.write('<main id="pokemonResults">');
+    pokemon.set.where({ q: 'series: Sword & Shield' }).then(result => {
+         for (let i = 0; i < result.data.length; i++) {
+            res.write ('<img src="' + result.data[i].images.small+ '">');
+        }
+    
+        res.write('</main>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    });
+    
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
-
+let SetsPage = require('./routes/SetsPage');
 
 let SearchPage = require('./routes/SearchPage'); 
+app.use('/SetsPage', SetsPage);
 
 app.use('/SearchPage', SearchPage);
 app.listen(3000, () => console.log('Server running on port 3000'));
